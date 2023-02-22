5e537147052cd85d5eeb86c93e3a9c2cbb26f7ec
hoon choi <photiary@gmail.com>
2023-02-22 23:20:29 +0900

    Token 정보를 localStorage에 저장

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 // modules
 import authModule from './modules/authModule'
 
 Vue.use(Vuex)
 
+// localStorage는 웹 브라우저를 닫았다 열어도 데이터가 남아있기 때문에,
+// PC와 같은 환경에서는 sessionStorage 사용을 검토한다.
+// Web storage 참조 링크: https://developer.mozilla.org/ko/docs/Web/API/Web_Storage_API
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+  reducer: state => ({
+    // 새로고침 후, 유지하고 싶은 state만 저장
+    authModule: state.authModule
+  })
+})
+
 export default new Vuex.Store({
   state: {},
-  modules: { authModule }
+  modules: { authModule },
+  plugins: [vuexLocalStorage.plugin]
 })
